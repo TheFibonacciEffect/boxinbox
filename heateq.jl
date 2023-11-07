ae7b12515d46be113fe28a60bf5ac2c1ab92f568
Caspar Gutsche <caspargutsche1@gmail.com>
2023-11-07 22:25:35 +0100

    try 2D

--- heateq.jl
+++ heateq.jl
@@ -2,57 +2,56 @@ const USE_GPU = false
 using ImplicitGlobalGrid
 import MPI
 using ParallelStencil
-using ParallelStencil.FiniteDifferences3D
+using ParallelStencil.FiniteDifferences2D
 using Plots
 
 @static if USE_GPU
-    @init_parallel_stencil(CUDA, Float64, 3);
+    @init_parallel_stencil(CUDA, Float64, 2);
 else
-    @init_parallel_stencil(Threads, Float64, 3);
+    @init_parallel_stencil(Threads, Float64, 2);
 end
 
-@parallel function diffusion3D_step!(T2, T, Ci, lam, dt, dx, dy, dz)
-    @inn(T2) = @inn(T) + dt*(lam*@inn(Ci)*(@d2_xi(T)/dx^2 + @d2_yi(T)/dy^2 + @d2_zi(T)/dz^2));
+@parallel function diffusion2D_step!(T2, T, Ci, lam, dt, dx, dy)
+    @inn(T2) = @inn(T) + dt*(lam*@inn(Ci)*(@d2_xi(T)/dx^2 + @d2_yi(T)/dy^2));
     return
 end
 
-function diffusion3D()
+function diffusion2D()
 # Physics
 lam        = 1.0;                                        # Thermal conductivity
 cp_min     = 1.0;                                        # Minimal heat capacity
-lx, ly, lz = 10.0, 10.0, 10.0;                           # Length of domain in dimensions x, y and z.
+lx, ly     = 10.0, 10.0;                                 # Length of domain in dimensions x, y and z.
 
 # Numerics
-nx, ny, nz = 256, 256, 256;                              # Number of gridpoints dimensions x, y and z.
+nx, ny = 256, 256;                              # Number of gridpoints dimensions x, y and z.
 nt         = 100;                                        # Number of time steps
-init_global_grid(nx, ny, nz);
+init_global_grid(nx, ny, 0);
 dx         = lx/(nx_g()-1);                              # Space step in x-dimension
 dy         = ly/(ny_g()-1);                              # Space step in y-dimension
-dz         = lz/(nz_g()-1);                              # Space step in z-dimension
 
 # Array initializations
-T   = @zeros(nx, ny, nz);
-T2  = @zeros(nx, ny, nz);
-Ci  = @zeros(nx, ny, nz);
+T   = @zeros(nx, ny);
+T2  = @zeros(nx, ny);
+Ci  = @zeros(nx, ny);
 
 # Initial conditions (heat capacity and temperature with two Gaussian anomalies each)
-Ci .= 1.0./( cp_min .+ Data.Array([5*exp(-((x_g(ix,dx,Ci)-lx/1.5))^2-((y_g(iy,dy,Ci)-ly/2))^2-((z_g(iz,dz,Ci)-lz/1.5))^2) +
-                                   5*exp(-((x_g(ix,dx,Ci)-lx/3.0))^2-((y_g(iy,dy,Ci)-ly/2))^2-((z_g(iz,dz,Ci)-lz/1.5))^2) for ix=1:size(T,1), iy=1:size(T,2), iz=1:size(T,3)]) )
-T  .= Data.Array([100*exp(-((x_g(ix,dx,T)-lx/2)/2)^2-((y_g(iy,dy,T)-ly/2)/2)^2-((z_g(iz,dz,T)-lz/3.0)/2)^2) +
-                   50*exp(-((x_g(ix,dx,T)-lx/2)/2)^2-((y_g(iy,dy,T)-ly/2)/2)^2-((z_g(iz,dz,T)-lz/1.5)/2)^2) for ix=1:size(T,1), iy=1:size(T,2), iz=1:size(T,3)])
+Ci .= 1.0./( cp_min .+ Data.Array([5*exp(-((x_g(ix,dx,Ci)-lx/1.5))^2-((y_g(iy,dy,Ci)-ly/2))^2) +
+                                   5*exp(-((x_g(ix,dx,Ci)-lx/3.0))^2-((y_g(iy,dy,Ci)-ly/2))^2) for ix=1:size(T,1), iy=1:size(T,2)]) )
+T  .= Data.Array([100*exp(-((x_g(ix,dx,T)-lx/2)/2)^2-((y_g(iy,dy,T)-ly/2)/2)^2) +
+                   50*exp(-((x_g(ix,dx,T)-lx/2)/2)^2-((y_g(iy,dy,T)-ly/2)/2)^2) for ix=1:size(T,1), iy=1:size(T,2)])
 T2 .= T;                                                 # Assign also T2 to get correct boundary conditions.
 
 # Time loop
-dt = min(dx^2,dy^2,dz^2)*cp_min/lam/8.1;                 # Time step for the 3D Heat diffusion
+dt = min(dx^2,dy^2)*cp_min/lam/8.1;                 # Time step for the 2D Heat diffusion
 for it = 1:nt
-    @parallel diffusion3D_step!(T2, T, Ci, lam, dt, dx, dy, dz);
+    @parallel diffusion2D_step!(T2, T, Ci, lam, dt, dx, dy);
     update_halo!(T2);
     T, T2 = T2, T;
 end
 
-heatmap(T[:,:,nz/2], aspect_ratio=1, color=:viridis, cbar=false, title="Temperature distribution")
+heatmap(T[:,:], aspect_ratio=1, color=:viridis, cbar=false, title="Temperature distribution")
 
 finalize_global_grid();
 end
 
-diffusion3D()
+diffusion2D()
